22cd052e2c75ac5319185c0160cdbf8373d152c4
Arthur <wangzhiqing@ata.net.cn>
2014-01-19 10:39:58 +0800

    change name

--- thrift/src/main/java/me/largetalk/morass/client/NBClient.java
+++ thrift/src/main/java/me/largetalk/morass/client/NBClient.java
@@ -1,22 +1,22 @@
 package me.largetalk.morass.client;
 
+/**
+ * Created by largetalk on 1/19/14.
+ */
 import me.largetalk.morass.thrift.Ernie;
 import me.largetalk.morass.thrift.Impression;
 import me.largetalk.morass.thrift.LuckAd;
 import org.apache.thrift.TException;
-import org.apache.thrift.async.AsyncMethodCallback;
-import org.apache.thrift.async.TAsyncClientManager;
 import org.apache.thrift.protocol.TCompactProtocol;
-import org.apache.thrift.transport.*;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-
-/**
- * Created by largetalk on 1/19/14.
- */
-public class AsyncClient {
+public class NBClient {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
 
     private long startTime, endTime;
@@ -24,30 +24,30 @@ public class AsyncClient {
     public void startClient() {
         startTime = System.nanoTime();
 
+        TTransport transport;
         try {
+            transport = new TFramedTransport(new TSocket("localhost", 9090, 300));
 
             System.out.println("client .....");
 
-            Ernie.AsyncClient client = new Ernie.AsyncClient(new TCompactProtocol.Factory(),
-                    new TAsyncClientManager(),
-                    new TNonblockingSocket("localhost", 9090));
-
+            TProtocol protocol = new TCompactProtocol(transport);
+            Ernie.Client client = new Ernie.Client(protocol);
+            transport.open();
 
             for(int i=100; i>0; i--) {
                 int randomInt = (int) (Math.random() * 10);
                 Impression impl = new Impression(String.valueOf(randomInt) , "pageurl", "ip");
-                client.bet(impl, new BetMethodCallback());
-
+                LuckAd luckAd = client.bet(impl);
+                System.out.println(luckAd.toString());
             }
 
+            transport.close();
         } catch (TTransportException ex) {
             ex.printStackTrace();
             logger.error(ex.getMessage());
         } catch (TException ex) {
             ex.printStackTrace();
             logger.error(ex.getMessage());
-        } catch (IOException ex) {
-            ex.printStackTrace();
         } finally {
             endTime = System.nanoTime();
             logger.info("client total time {} 微秒", (endTime - startTime)/1000);
@@ -60,21 +60,5 @@ public class AsyncClient {
         NBClient client = new NBClient();
 
         client.startClient();
-    }
-
-    class BetMethodCallback implements AsyncMethodCallback<Ernie.AsyncClient.bet_call> {
-        public void onComplete(Ernie.AsyncClient.bet_call bet_call) {
-            try {
-                LuckAd luckAd = bet_call.getResult();
-                System.out.println(luckAd.toString());
-            } catch (TException ex) {
-                ex.printStackTrace();
-            }
-
-        }
-
-        public void onError(Exception ex) {
-            ex.printStackTrace();
-        }
     }
 }
