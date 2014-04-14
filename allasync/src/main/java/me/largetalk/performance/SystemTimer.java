b12274edc6c42f54716f2cc1884206bd483eb529
Arthur <arthur_wang@adsame.com>
2014-04-14 12:14:01 +0800

    update timer

---
+++ allasync/src/main/java/me/largetalk/performance/SystemTimer.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) AdSame Corporation. All rights reserved.
+ */
+package me.largetalk.performance;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SystemTimer {
+    
+    private final static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private final static long tickUnit = Long.parseLong(System.getProperty("notify.systimer.tick", "1"));
+    private static volatile long time = System.currentTimeMillis();
+    
+    private static class TimerTicker implements Runnable {
+        public void run() {
+            time = System.currentTimeMillis();
+        }
+    }
+    
+    public static long currentTimeMillis() {
+        return time;
+    }
+    
+    static {
+        executor.scheduleAtFixedRate(new TimerTicker(), tickUnit, tickUnit, TimeUnit.MILLISECONDS);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                executor.shutdown();
+            }
+        });
+    }
+}
--- allasync/src/main/java/me/largetalk/performance/TestTimer.java
+++ allasync/src/main/java/me/largetalk/performance/TestTimer.java
@@ -6,11 +6,35 @@ package me.largetalk.performance;
 public class TestTimer {
 
     public static void main(String args[]) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(SystemTimer.currentTimeMillis() + " - " +
+                    System.currentTimeMillis() + " => " +
+                    (SystemTimer.currentTimeMillis() - System.currentTimeMillis()));
+            try {
+                Thread.sleep(65);
+            } catch (InterruptedException ex) {}
+        }
+        
+        long last = 0;
         long start = System.currentTimeMillis();
+        System.out.println(start);
         for (int i = 0; i < 10000000; i++) {
-            System.currentTimeMillis();
+            last = System.currentTimeMillis();
         }
         long end = System.currentTimeMillis();
-        System.out.println(end-start);
+        System.out.println(end - start);
+        System.out.println(last);
+        System.out.println("=============");
+
+        start = System.currentTimeMillis();
+        System.out.println(start);
+        for (int i = 0; i < 10000000; i++) {
+            last = SystemTimer.currentTimeMillis();
+        }
+        end = System.currentTimeMillis();
+        System.out.println(end - start);
+        System.out.println(last);
+        
+        System.exit(0);
     }
 }
